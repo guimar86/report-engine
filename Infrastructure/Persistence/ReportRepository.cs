b4b53983d9eb90071c1308b61d13dc95a90514e4
Renato Martins <guillenmartins@gmail.com>
2026-03-24 00:17:11 +0000

    Added mock response

--- Infrastructure/Persistence/ReportRepository.cs
+++ Infrastructure/Persistence/ReportRepository.cs
@@ -9,6 +9,12 @@ public class ReportRepository(ReportDbContext context) : IReportRepository
 {
     public ReportData GetReportData(string id)
     {
-        return context.Reports.AsNoTracking().FirstOrDefault(r => r.Id == id) ?? throw new KeyNotFoundException();
+
+        return new ReportData
+        {
+            Id = id,
+            Payload = $"{{\"Name\":\"John Doe\",\"Items\":[{{\"Name\":\"Laptop\",\"Price\":1200.50}},{{\"Name\":\"Mouse\",\"Price\":25.99}},{{\"Name\":\"Keyboard\",\"Price\":75.00}}],\"Total\":1301.49}}"
+        };
+        //return context.Reports.AsNoTracking().FirstOrDefault(r => r.Id == id) ?? throw new KeyNotFoundException();
     }
 }
